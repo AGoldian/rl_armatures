5e4254196d5eb4464c5b88bc0f0ab85ed78121bd
v.d.olennikov <v.d.olennikov@tinkoff.ru>
2023-06-24 20:55:10 +0300

    new alg

--- optimal_purchase.py
+++ optimal_purchase.py
@@ -1,22 +1,40 @@
+import math
+
+
 def optimal_purchase(prices, max_items):
-    # определяем стартовое значение запаса товара как 0
-    current_items = 0
     days_number = len(prices)
+    spent = [math.inf] * days_number
+    buy_to_from = [{} for _ in range(days_number)]
+    buy_from = [0] * days_number
 
-    bought = [0] * days_number  # список количества купленного товара по дням
-    remain = [0] * days_number  # список оставшегося товара на каждый день
+    spent[0] = prices[0]
+    buy_to_from[0][0] = 1
+    for dayInd in range(1, days_number):
+        spent[dayInd] = spent[dayInd - 1] + prices[dayInd]
 
-    for day in range(days_number):
-        max_period_to_predict = min(max_items, days_number - day)
-        if current_items >= days_number - day:
-            max_period_to_predict = 0
-        quantity_to_buy = 1 if current_items == 0 else 0
-        for i in range(1, max_period_to_predict):
-            if prices[day + i] > prices[day]:
-                quantity_to_buy += 1
-            else:
-                break
-        current_items += quantity_to_buy - 1
-        bought[day] = quantity_to_buy
-        remain[day] = current_items
-    return bought, remain
+        buy_to_from[dayInd][dayInd] = 1
+        buy_from[dayInd] = dayInd
+        for buy_from_day in range(max(0, dayInd - max_items + 1), dayInd):
+            days_left = dayInd - buy_from_day
+            need_to_spent = spent[buy_from_day] + prices[buy_from_day] * days_left
+            if spent[dayInd] > need_to_spent:
+                spent[dayInd] = need_to_spent
+                buy_to_from[buy_from_day][dayInd] = days_left + 1
+                buy_from[dayInd] = buy_from_day
+
+    bought = [0] * days_number
+    ind = days_number - 1
+    while ind >= 0:
+        bought[buy_from[ind]] = buy_to_from[buy_from[ind]][ind]
+        if buy_from[ind] == ind:
+            ind = ind - 1
+        else:
+            ind = buy_from[ind]
+
+    remain_items = [0] * days_number
+    current_items = 0
+    for i in range(days_number):
+        current_items += bought[i]
+        current_items -= 1
+        remain_items[i] = current_items
+    return bought, remain_items
--- test_optimal_purchase.py
+++ test_optimal_purchase.py
@@ -37,6 +37,12 @@ class Test(TestCase):
             [1, 6, 0, 0, 0, 0, 0],
             [0, 5, 4, 3, 2, 1, 0]
         ],
+        [
+            5,
+            [10, 3, 2, 6, 7, 8, 1, 3, 99, 19, 2, 122, 123, 45, 5, 5],
+            [1, 1, 4, 0, 0, 0, 5, 0, 0, 0, 4, 0, 0, 0, 1, 0],
+            [0, 0, 3, 2, 1, 0, 4, 3, 2, 1, 4, 3, 2, 1, 0, 0],
+        ],
     ])
     def test_optimal_purchase(self, max_items, prices, expected_bought, expected_left):
         bought, left = optimal_purchase(prices, max_items)
