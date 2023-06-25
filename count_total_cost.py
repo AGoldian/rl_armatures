ecda4dc88a298dc2cda0fc26f6edb9278d51f8c2
v.d.olennikov <v.d.olennikov@tinkoff.ru>
2023-06-25 08:03:27 +0300

    fix req.txt

--- count_total_cost.py
+++ count_total_cost.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 from optimal_purchase import optimal_purchase
 
-df = pd.read_csv("train_dataframe.csv")
+df = pd.read_csv("./data/train_dataframe.csv")
 prices = list(df['Цена на арматуру'])
+
 bought, remained = optimal_purchase(prices, 10)
 total_cost = sum([bought[i]*prices[i] for i in range(len(prices))])
-print(total_cost)
+
+print("Total cost:", total_cost)
